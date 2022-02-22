810f388091b9962e97bff083003d0b1d90a8233e
Arbaz86 <arbaz10042002@gmail.com>
2022-02-22 18:25:05 +0530

    There was a little change in style

---
+++ scripts/sign in.js
@@ -0,0 +1,9 @@
+let nameImg = document.getElementById("logo-img");
+nameImg.addEventListener("click", () => {
+    window.location.href = "index.html"
+});
+
+// goto Regitration page Btn
+function register() {
+
+}
