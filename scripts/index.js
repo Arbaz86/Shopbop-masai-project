aa7f9093084814ce6b63645d66a0f444d2b7f291
Arbaz86 <arbaz10042002@gmail.com>
2022-02-23 17:30:37 +0530

    Making progress on the landing page

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,91 @@ function goToSignInPage() {
 function goToRegisterPage() {
     window.location.href = "register.html";
 }
+
+let itmesData = [
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/ragbo/ragbo4587610328/ragbo4587610328_1645215598895_2-0._QL90_UX207_.jpg",
+        title: "Reg & Bone",
+        type: "Liana Cardigan",
+        price: "$359.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/mothr/mothr215131733b/mothr215131733b_1645465122812_2-0._QL90_UX207_.jpg",
+        title: "MOTHER",
+        type: "High Waisterd Rider Ank...",
+        price: "$198.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/loeff/loeff4191214a9b/loeff4191214a9b_1645222764978_2-0._QL90_UX207_.jpg",
+        title: "Loeffler Randall",
+        type: "Willa Bag",
+        price: "$295.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/rebec/rebec426021af4f/rebec426021af4f_1645216369565_2-0._QL90_UX207_.jpg",
+        title: "Redecca Taylor",
+        type: "Popover Dress",
+        price: "$265.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/ismdb/ismdb308411c5f0/ismdb308411c5f0_1645223941168_2-0._QL90_UX207_.jpg",
+        title: "Lsabel Marant",
+        type: "Elham Sandals",
+        price: "$790.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/rebec/rebec426061d360/rebec426061d360_1645459846835_2-0._QL90_UX207_.jpg",
+        title: "Rebecca Taylor",
+        type: "Poplin Jumpsuit",
+        price: "$395.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/gamer/gamer304431d3ca/gamer304431d3ca_1645464383960_2-0._QL90_UX207_.jpg",
+        title: "Good American",
+        type: "Modern Tank Thong Bodysuit.",
+        price: "$65.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/vince/vince515271d49b/vince515271d49b_1645221902427_2-0._QL90_UX207_.jpg",
+        title: "Vince",
+        type: "sculptural Panama Shorts",
+        price: "$275.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/rebec/rebec4260433159/rebec4260433159_1645216382268_2-0._QL90_UX207_.jpg",
+        title: "Rebecca Tylor",
+        type: "LongSleeve Dress",
+        price: "$295.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/mirem/mirem3020211739/mirem3020211739_1645460279658_2-0._QL90_UX207_.jpg",
+        title: "Maison Iren",
+        type: "Necklace Cortona",
+        price: "$170.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/jcque/jcque308281c5f0/jcque308281c5f0_1645463380121_2-0._QL90_UX207_.jpg",
+        title: "Jacquemus",
+        type: "Le Jacquemus T-Shirt",
+        price: "$140.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/ragbo/ragbo459051c5f0/ragbo459051c5f0_1641934868261_2-0._QL90_UX207_.jpg",
+        title: "Rag & Bone",
+        type: "Roxie Leggings",
+        price: "$225.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/cesbe/cesbe300631c925/cesbe300631c925_1639683175067_2-0._QL90_UX207_.jpg",
+        title: "Christopher Esber",
+        type: "Bandeau Crop Shirt",
+        price: "$420.00"
+    },
+    {
+        itemImg: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/prosh/prosh205271d22e/prosh205271d22e_1645465897005_2-0._QL90_UX207_.jpg",
+        title: "Proenza Schouler",
+        type: "Zebra Cotton Jacquard Skit",
+        price: "$890.00"
+    },
+];
+console.log(itmesData);
--- scripts/sign in.js
+++ scripts/sign in.js
@@ -7,3 +7,46 @@ nameImg.addEventListener("click", () => {
 function register() {
     window.location.href = "register.html"
 }
+
+// sign in working part begin here
+let usersData = JSON.parse(localStorage.getItem("UsersData")) || [];
+
+function signInBtn() {
+
+    let Email = document.getElementById("Email").value;
+    let Password = document.getElementById("Password").value;
+
+    let errInputEmail = document.querySelector(".errInputEmail");
+    let errInputPass = document.querySelector(".errInputPass");
+
+    if (Email == "" && Password == "") {
+        errInputEmail.style.display = "inline-block"
+        errInputPass.style.display = "inline-block"
+    }
+    else if (Email == "") {
+        errInputEmail.style.display = "inline-block"
+        errInputPass.style.display = "none"
+
+    }
+    else if (Password == "") {
+        errInputEmail.style.display = "none";
+        errInputPass.style.display = "inline-block"
+    }
+    else {
+        errInputEmail.style.display = "none";
+        errInputPass.style.display = "none";
+
+        for (var i = 0; i < usersData.length; i++) {
+            if (usersData[i].Email === Email && usersData[i].Password === Password) {
+                alert("login successful");
+                window.location.href = "index.html";
+                break;
+            }
+        }
+
+    }
+
+}
+// end here
+
+
